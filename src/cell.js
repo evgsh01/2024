0b03aa0ca7398b4cf780b30efc1f5b5130db2d68
evgsh01 <allorean@gmail.com>
2023-05-10 17:05:59 +0300

    случайная ячейка, цвет, перемещение вверх

--- src/cell.js
+++ src/cell.js
@@ -6,4 +6,30 @@ export class Cell {
         this.y = y;
         gridElement.append(cell);
     }
+
+    linkTile(tile) {
+        tile.setXY(this.x, this.y);
+        this.linkedTile = tile;
+    }
+
+    unlinkTile() {
+        this.linkedTile = null;
+    }
+
+    isEmpty() {
+        return !this.linkedTile;
+    }
+
+    linkTileForMerge(tile) {
+        tile.setXY(this.x, this.y);
+        this.linkedTileForMerge = tile;
+    }
+
+    hasTileForMerge() {
+        return !!this.linkedTileForMerge;
+    }
+
+    canAccept(newTile) {
+        return this.isEmpty() || (!this.hasTileForMerge() && this.linkedTile.value === newTile.value);
+    }
 }
--- src/grid.js
+++ src/grid.js
@@ -7,5 +7,21 @@ export class Grid {
         for (let i = 0; i < CELLS_COUNT; i++) {
             this.cells.push(new Cell(gridElement, (i % GRID_SIZE), Math.floor(i / GRID_SIZE)));                    
         }
+
+        this.cellsGroupedByColumn = this.groupCellsByColumn();
+    }
+
+    getRandomEmptyCell() {
+        const emptySells = this.cells.filter(cell => cell.isEmpty());
+        const randomIndex = Math.floor(Math.random() * emptySells.length);
+        return emptySells[randomIndex];
+    }
+
+    groupCellsByColumn() {
+        return this.cells.reduce((groupedCells, cell) => {
+            groupedCells[cell.x] = groupedCells[cell.x] || [];
+            groupedCells[cell.x][cell.y] = cell;
+            return groupedCells;
+        }, [])
     }
 }
